953551762cccad647219333e79706e1daa137344
Mamun128169 <mh4559641@gmail.com>
2023-10-07 22:33:04 +0600

    loaded the countries data with API

---
+++ src/components/Countries/Countries.jsx
@@ -0,0 +1,20 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
+const Countries = () => {
+    const [countries, SetCountries] = useState([]);
+    
+    useEffect(() => {
+        fetch("https://restcountries.com/v3.1/all")
+           .then(res => res.json())
+           .then(data => SetCountries(data))
+    }, [])
+
+    return (
+        <div>
+            <h2>Countries: {countries.length} </h2>
+        </div>
+    );
+};
+
+export default Countries;
